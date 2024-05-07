dcb2a0c60e56c16ce07c2557ecae71f510f416c5
Loïc Mathieu <loikeseke@gmail.com>
2024-05-07 18:01:44 +0200

    WIP

--- core/src/main/java/io/kestra/core/serializers/JacksonMapper.java
+++ core/src/main/java/io/kestra/core/serializers/JacksonMapper.java
@@ -2,12 +2,7 @@
 package io.kestra.core.serializers;
 
 import com.amazon.ion.IonSystem;
-import com.amazon.ion.impl._Private_IonBinaryWriterBuilder;
-import com.amazon.ion.impl._Private_Utils;
-import com.amazon.ion.system.IonBinaryWriterBuilder;
-import com.amazon.ion.system.IonReaderBuilder;
-import com.amazon.ion.system.IonTextWriterBuilder;
-import com.amazon.ion.system.SimpleCatalog;
+import com.amazon.ion.system.*;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -145,34 +140,9 @@ public final class JacksonMapper {
     }
 
     private static IonSystem createIonSystem() {
-        // This code is inspired by the IonSystemBuilder#build() method and the usage of "withWriteTopLevelValuesOnNewLines(true)".
-        //
-        // After the integration of the relevant pull request (https://github.com/amazon-ion/ion-java/pull/781),
-        // it is expected that this code should be replaced with a more simplified version.
-        //
-        // The simplified code would look like below:
-        //
-        // return IonSystemBuilder.standard()
-        //    .withIonTextWriterBuilder(IonTextWriterBuilder.standard().withWriteTopLevelValuesOnNewLines(true))
-        //    .build();
-        //
-        // TODO: Simplify this code once the pull request is integrated.
-
-        final var catalog = new SimpleCatalog();
-
-        final var textWriterBuilder = IonTextWriterBuilder.standard()
-            .withCatalog(catalog)
-            .withCharsetAscii()
-            .withWriteTopLevelValuesOnNewLines(true); // write line separators on new lines instead of spaces
-
-        final var binaryWriterBuilder = IonBinaryWriterBuilder.standard()
-            .withCatalog(catalog)
-            .withInitialSymbolTable(_Private_Utils.systemSymtab(1));
-
-        final var readerBuilder = IonReaderBuilder.standard()
-            .withCatalog(catalog);
-
-        return newLiteSystem(textWriterBuilder, (_Private_IonBinaryWriterBuilder) binaryWriterBuilder, readerBuilder);
+         return IonSystemBuilder.standard()
+            .withIonTextWriterBuilder(IonTextWriterBuilder.standard().withWriteTopLevelValuesOnNewLines(true))
+            .build();
     }
 
     public static Pair<JsonNode, JsonNode> getBiDirectionalDiffs(Object previous, Object current)  {
